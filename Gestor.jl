ac8645957d4f0629430c40ededf63536539c0573
Nicolle Girón <jackelyngironvillacinda@gmail.com>
2025-04-24 20:55:26 -0600

    Se agregaron las funciones de completar y el menu principal

--- Gestor.jl
+++ Gestor.jl
@@ -52,9 +52,83 @@ function listarxPrioridad()
         ordenadas = sort(lista, by = t -> t.fecha)
         for (i, t) in enumerate(ordenadas)
             estado_str = t.estado ? "Completada" : "Pendiente"
-            println("[$i] $(t.titulo) - $(t.fecha) - $(estado_str) - Categoria: $(collect(t.categoria))")
+            println("[$i] $(t.titulo) - $(t.fecha) - $(estado_str) - Categoria: $(t.categoria)")
         end
     end
 end
 
+# Marcar tarea como completada
+function completarT()
+    println("Ingrese la prioridad de la tarea (A, M, B):")
+    prioridad = uppercase(readline())
 
+    if !haskey(tareas, prioridad)
+        println("Prioridad no válida.")
+        return
+    end
+    if isempty(tareas[prioridad])
+        println("No hay tareas en esta prioridad.")
+        return
+    end
+
+    println("Tareas pendientes en la prioridad $(prioridad):")
+    tareasPendientes = filter(t -> !t.estado, tareas[prioridad])
+    if isempty(tareasPendientes)
+        println("No hay tareas pendientes en esta prioridad.")
+        return
+    end
+    for (i, t) in enumerate(tareasPendientes)
+        println("$(i) $(t.titulo) - $(Dates.format(t.fecha, "dd-mm-yyyy"))")
+    end
+
+    println("Ingrese el numero de la tarea a completar:")
+    try
+        indice = parse(Int, readline())
+        if indice < 1 || indice > length(tareasPendientes)
+            println("Numero de tarea no valido")
+            return
+        end
+        tareaCompletar = tareasPendientes[indice]
+        tareaCompletar.estado = true
+        println("Tarea marcada como completada: $(tareaCompletar.titulo)")
+    catch e
+        println("Error al ingresar el número de tarea:")
+    end
+
+end
+
+
+function menu()
+    while true
+        println("\n Gestor de Tareas")
+        println("1. Agregar tarea")
+        println("2. Listar tareas por prioridad")
+        println("3. Listar tareas por fecha")
+        println("4. Listar tareas por etiqueta")
+        println("5. Marcar tarea como completada")
+        println("6. Salir")
+        print("Ingrese la opcion que desea: ")
+        
+        opcion = readline()
+
+        if opcion == "1"
+            agregarT()
+        elseif opcion == "2"
+            listarxPrioridad()
+        elseif opcion == "3"
+            
+        elseif opcion == "4"
+            
+        elseif opcion == "5"
+            completarT()
+        elseif opcion == "6"
+            println(" Cerrando el programa")
+            break
+        else
+            println(" Opción no válida.")
+        end
+    end
+end
+
+# Iniciar el programa
+menu()
